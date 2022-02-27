f6a0add970cde6bb8806f2e573a834354bd82996
beksultan <turgunbaaev@gmail.com>
2022-02-28 00:33:19 +0600

    Did the mobile version

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,31 +7,20 @@ $(document).ready(function() {
         slidesToScroll: 1,
         driggable: true,
         nextArrow: ".right-arrow",
-        prevArrow: ".left-arrow"
-        // responsive: [
-        //     {
-        //     breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             slidesToScroll: 3,
-        //             infinite: true,
-        //             dots: true
-        //         }
-        //     },
-        //     {
-        //     breakpoint: 600,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 2
-        //         }
-        //     },
-        //     {
-        //     breakpoint: 480,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1
-        //         }
-        //     }
-        // ]
-        });
+        prevArrow: ".left-arrow",
+        responsive: [
+            {
+            breakpoint: 991,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+            breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
+    });
 });
