ed4fa6662df0e9c78f8c4a077ba75c81aaa36e82
beksultan <turgunbaaev@gmail.com>
2022-03-07 00:11:42 +0600

    Сделал моб версию страницы СКИДКИ

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Slider:
     $('.slider_wrapper').slick({
         dots: false,
         infinite: true,
@@ -23,6 +24,22 @@ $(document).ready(function() {
             },
         ]
     });
+
+   // Input mask:
+   $('.phone_mask').inputmask("phone", {
+        placeholder: '#',
+        showMaskOnHover: false,
+    });
+        
+        Inputmask.extendAliases({
+        my_phone: {
+            alias: "+7(###)###-##-##",
+            phoneCodes: [{
+            mask: "+7(###)###-##-##",
+            }],
+        }
+    });
+    $('.phone_mask').inputmask("my_phone");
 });
 
 // Mobile menu:
