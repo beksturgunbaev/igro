6223dd77d6f4b2f75673c75745ccd6b6c766f5a5
beksultan <turgunbaaev@gmail.com>
2022-04-07 03:16:38 +0600

    cv  cv

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,111 +1,112 @@
-$(document).ready(function() {
-    // Slider:
-    $('.slider_wrapper').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        driggable: true,
-        nextArrow: ".right-arrow",
-        prevArrow: ".left-arrow",
-        responsive: [
-            {
-            breakpoint: 991,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-            breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-        ]
-    });
+$(document).ready(function () {
+  // Input mask:
+  $(".phone_mask").inputmask("phone", {
+    placeholder: "#",
+    showMaskOnHover: false,
+  });
 
-   // Input mask:
-   $('.phone_mask').inputmask("phone", {
-        placeholder: '#',
-        showMaskOnHover: false,
-    });
-        
-        Inputmask.extendAliases({
-        my_phone: {
-            alias: "+7(###)###-##-##",
-            phoneCodes: [{
-            mask: "+7(###)###-##-##",
-            }],
-        }
-    });
-    $('.phone_mask').inputmask("my_phone");
+  Inputmask.extendAliases({
+    my_phone: {
+      alias: "+7(###)###-##-##",
+      phoneCodes: [
+        {
+          mask: "+7(###)###-##-##",
+        },
+      ],
+    },
+  });
+  $(".phone_mask").inputmask("my_phone");
+
+// Slider:
+$(".slider_wrapper").slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    driggable: true,
+    nextArrow: ".right-arrow",
+    prevArrow: ".left-arrow",
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });  
 });
 
 // Mobile menu:
-let burgerBtn = document.querySelector('.burger_menu'),
-    closeBtn = document.querySelector('.close_btn'),
-    modal = document.querySelector('.modal'),
-    mobileMenu = document.querySelector('.mobile_menu'),
-    bodyScroll = document.querySelector('body');
+let burgerBtn = document.querySelector(".burger_menu"),
+  closeBtn = document.querySelector(".close_btn"),
+  modal = document.querySelector(".modal"),
+  mobileMenu = document.querySelector(".mobile_menu"),
+  bodyScroll = document.querySelector("body");
 
-burgerBtn.onclick = function() {
-    modal.classList.add('active');
-    mobileMenu.classList.add('active');
-    bodyScroll.classList.add('no_scroll');
-}
-closeBtn.onclick = function() {
-    modal.classList.remove('active');
-    mobileMenu.classList.remove('active');
-    bodyScroll.classList.remove('no_scroll');
-}
+burgerBtn.onclick = function () {
+  modal.classList.add("active");
+  mobileMenu.classList.add("active");
+  bodyScroll.classList.add("no_scroll");
+};
+closeBtn.onclick = function () {
+  modal.classList.remove("active");
+  mobileMenu.classList.remove("active");
+  bodyScroll.classList.remove("no_scroll");
+};
 
 // Accordion:
 const acc = document.getElementsByClassName("catalog_link");
 
 for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle('active');
-        let panel = this.nextElementSibling;
-        let icon = this.querySelector('svg');
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-            icon.classList.remove('active');
-
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-            icon.classList.add('active');
-        }
-    });
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    let icon = this.querySelector("svg");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      icon.classList.remove("active");
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      icon.classList.add("active");
+    }
+  });
 }
 
 // Dropdown
-let dropdown = document.querySelectorAll('.dropp_down');
+let dropdown = document.querySelectorAll(".dropp_down");
 
-for(let i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener('click', function() {
-        this.nextElementSibling.classList.toggle('active');
-    });
+for (let i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("active");
+  });
 }
 
-let city = document.querySelector('.cities_select span div');
-let cityUl = document.querySelector('.cities_option');
-let cityOption = document.querySelectorAll('.cities_option li');
+let city = document.querySelector(".cities_select span div");
+let cityUl = document.querySelector(".cities_option");
+let cityOption = document.querySelectorAll(".cities_option li");
 
-for(let i = 0; i < cityOption.length; i++) {
-    cityOption[i].addEventListener('click', function() {
-        city.innerText = this.innerText;
-        cityUl.classList.remove('active');
-    });
+for (let i = 0; i < cityOption.length; i++) {
+  cityOption[i].addEventListener("click", function () {
+    city.innerText = this.innerText;
+    cityUl.classList.remove("active");
+  });
 }
 
 //  Choose file:
 
-updateList = function() {
-  let input = document.getElementById('file');
-  let output = document.getElementById('fileList');
+updateList = function () {
+  let input = document.getElementById("file");
+  let output = document.getElementById("fileList");
 
   for (var i = 0; i < input.files.length; ++i) {
-    output.innerHTML += '<p>' + input.files.item(i).name + '</p>';
+    output.innerHTML += "<p>" + input.files.item(i).name + "</p>";
   }
-}
+};
